16cb9e9179d84bc027803c332f266af935c1d8c8
sweet04 <gozalovash@gmail.com>
2021-10-13 09:39:52 +0400

    lab2 first commit

---
+++ src/main/java/PermissionManager.java
@@ -0,0 +1,36 @@
+enum PermissionLevel{
+    ADMIN,
+    DEVELOPER,
+    USER
+}
+
+public class PermissionManager {
+    private PermissionLevel mCurrentLevel = PermissionLevel.USER;
+
+    public String getPermissionLevel(PermissionLevel pLevel){
+        if (pLevel == PermissionLevel.ADMIN){
+            return "Admin";
+        }
+        if (pLevel == PermissionLevel.DEVELOPER){
+            return "Developer";
+        }
+        if (pLevel == PermissionLevel.USER){
+            return "User";
+        }
+        return "EMPTY";
+    }
+
+    private void setPermissionLevel(PermissionLevel pLevel){
+        mCurrentLevel = pLevel;
+    }
+
+    public static void main(String[] args) {
+        PermissionManager pmanager = new PermissionManager();
+        System.out.println(pmanager.getPermissionLevel(pmanager.mCurrentLevel));
+        pmanager.setPermissionLevel(PermissionLevel.ADMIN);
+        System.out.println(pmanager.getPermissionLevel(pmanager.mCurrentLevel));
+    }
+
+
+
+}
---
+++ src/test/java/PermissionManagerTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PermissionManagerTest extends PermissionManager {
+
+    @Test
+    void testGetPermissionLevel() {
+        assertTrue(true);
+    }
+
+    @Test
+    void testMain() {
+        assertTrue(true);
+    }
+
+}
