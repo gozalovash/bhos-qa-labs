6c2b17f0b2dfcfd44d834f87417615459c8d96ba
sweet04 <gozalovash@gmail.com>
2021-10-31 15:53:19 +0400

    -- Decrypt.sh changed

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASE_FILE_KEY" \
---output $HOME/secrets/spring-project-3-firebase-adminsdk-dang2-734dba00f4.json spring-project-3-firebase-adminsdk-dang2-734dba00f4.json.gpg
+--output ./spring-project-3-firebase-adminsdk-dang2-734dba00f4.json spring-project-3-firebase-adminsdk-dang2-734dba00f4.json.gpg
