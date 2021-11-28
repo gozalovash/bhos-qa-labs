1a94e1f6290062b5afdc15fe003c591906faaeec
sweet04 <gozalovash@gmail.com>
2021-11-28 17:40:22 +0400

    --Initial commit, decrypt_sh, gradle.yml

---
+++ src/test/java/com/example/springproj5/Springproj5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springproj5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springproj5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
